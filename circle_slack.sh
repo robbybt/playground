4bf37a23781ce4285c463389376a9e1ec14319f7
robbybt <robby.budianto@tokopedia.com>
2018-11-16 17:54:34 +0700

    asd

--- circle_slack.sh
+++ circle_slack.sh
@@ -6,9 +6,9 @@ Github   : $CIRCLE_PULL_REQUEST
 <!channel>
 "
 
-if [ "$CIRCLE_BRANCH" != "ggzzzz" ] || [ "$CIRCLE_BRANCH" != "staging" ] ; then
+if [ "$CIRCLE_BRANCH" != "ggzzzz" ] && [ "$CIRCLE_BRANCH" != "staging" ] ; then
     curl -X POST --data-urlencode "payload={\"channel\": \"#testrobby2\", \"username\": \"Robby\", \"text\": \"$msg\",\"link_names\": 1, \"icon_emoji\": \":davidjenggot:\"}" https://hooks.slack.com/services/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
-    echo "already slack"
+    echo "\nalready slack"
 else
     echo "no slack"
 fi
