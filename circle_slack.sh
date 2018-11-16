4855086d772ba3d0ce17f3dd47326e51320f6dd9
robbybt <robby.budianto@tokopedia.com>
2018-11-16 17:59:36 +0700

    asdg

--- circle_slack.sh
+++ circle_slack.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 msg="\
-Halo, saya $CIRCLE_USERNAME mau meminta review untuk PR berikut
+Halo, saya $CIRCLE_USERNAME mau meminta review untuk PR berikut <!@cart-dev>
 Github   : $CIRCLE_PULL_REQUEST
 <!channel>
 "
 
-if [ "$CIRCLE_BRANCH" != "ggzzzz" ] && [ "$CIRCLE_BRANCH" != "staging" ] ; then
+if [ "$CIRCLE_BRANCH" != "development" ] && [ "$CIRCLE_BRANCH" != "staging" ] ; then
     curl -X POST --data-urlencode "payload={\"channel\": \"#testrobby2\", \"username\": \"Robby\", \"text\": \"$msg\",\"link_names\": 1, \"icon_emoji\": \":davidjenggot:\"}" https://hooks.slack.com/services/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
     echo "\nalready slack"
 else
