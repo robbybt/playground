a4e626e295c13d51274e13bf0e4bb9185f0ebc68
robbybt <robby.budianto@tokopedia.com>
2018-11-16 17:47:07 +0700

    asdasd

--- circle_slack.sh
+++ circle_slack.sh
@@ -4,10 +4,9 @@ msg="\
 Halo, @viriyahendarta
 Username : $CIRCLE_USERNAME
 Github   : $CIRCLE_PULL_REQUEST
-
 <!channel>
 "
 
-
-
-curl -X POST --data-urlencode "payload={\"channel\": \"#testrobby2\", \"username\": \"Robby\", \"text\": \"$msg\",\"link_names\": 1, \"icon_emoji\": \":davidjenggot:\"}" https://hooks.slack.com/services/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
+if [ "$CIRCLE_BRANCH" != "ggzzzz" ] || [ "$CIRCLE_BRANCH" != "staging" ] ; then
+    curl -X POST --data-urlencode "payload={\"channel\": \"#testrobby2\", \"username\": \"Robby\", \"text\": \"$msg\",\"link_names\": 1, \"icon_emoji\": \":davidjenggot:\"}" https://hooks.slack.com/services/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
+fi
