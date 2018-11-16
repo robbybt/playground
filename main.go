ea221443fe3d2068c5ae3f6c282ee451b6c8ab0e
robbybt <robby.budianto@tokopedia.com>
2018-11-16 17:02:08 +0700

    zcv

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ func init() {
 
 func main(){
 	//asdsad
-	//echo "$(cat "test.out" | grep -a "^FAIL\s")"
+	//echo "$(cat "test.out" | grep -a "^FAIL\s")"a
 	gg := *test
 	fmt.Println(gg)
 	if gg < 5{
