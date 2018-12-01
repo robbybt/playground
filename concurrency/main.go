bb66e0f8e50564524b613eed649f35c30f8b712c
robbybt <robby.budianto@tokopedia.com>
2018-12-01 20:53:53 +0700

    [fix] asd

--- concurrency/main.go
+++ concurrency/main.go
@@ -1,1 +1,27 @@
-package concurrency
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+type asd struct{
+	text string
+}
+type cBreakersObj struct{
+	list map[string]asd
+}
+
+var cBreakers cBreakersObj
+func (c *cBreakersObj) setCircuitBreaker(list []string, v *asd) {
+	if c.list == nil {
+		c.list = make(map[string]asd)
+	}
+	for _, cbList := range list {
+		c.list[cbList] = *v
+	}
+}
+func main(){
+	cBreakers.SetUpCircuitBreaker([]string{"a","b"},&asd{text:"zz"})
+	fmt.Println(cBreakers.list)
+}
